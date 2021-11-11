a4537d3d5662fc02ba01207849647d155de8254c
Leonardo de Macedo Sartorello <leo.sartorello98@gmail.com>
2021-11-11 18:52:55 +0000

    Aula_2

--- infra/variables.tf
+++ infra/variables.tf
@@ -11,3 +11,15 @@ variable "instancia" {
 variable "grupoDeSeguranca" {
   type = string
 }
+
+variable "minimo" {
+  type = number
+}
+
+variable "maximo" {
+  type = number
+}
+
+variable "nomeGrupo" {
+  type = string
+}
