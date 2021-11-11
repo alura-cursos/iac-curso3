037b8f188ababd8376aefb327e280f94e078a24a
Leonardo de Macedo Sartorello <leo.sartorello98@gmail.com>
2021-11-11 21:57:58 +0000

    Aula_3

--- infra/main.tf
+++ infra/main.tf
@@ -32,12 +32,45 @@ resource "aws_key_pair" "chaveSSH" {
 
 
 resource "aws_autoscaling_group" "grupo" {
-  availability_zones = [ "${var.regiao_aws}a" ]
+  availability_zones = [ "${var.regiao_aws}a", "${var.regiao_aws}b" ]
   name = var.nomeGrupo
   max_size = var.maximo
   min_size = var.minimo
   launch_template {
     id = aws_launch_template.maquina.id
     version = "$Latest"
+  }
+}
+
+resource "aws_default_subnet" "subnet_1" {
+  availability_zone = "${var.regiao_aws}a" 
+}
+
+resource "aws_default_subnet" "subnet_2" {
+  availability_zone = "${var.regiao_aws}b" 
+}
+
+resource "aws_lb" "loadBalancer" {
+  internal = false
+  subnets = [ aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id ]
+}
+
+resource "aws_default_vpc" "vpc" {
+}
+
+resource "aws_lb_target_group" "alvoLoadBalancer" {
+  name = "alvoLoadBalancer"
+  port = "8000"
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.vpc.id
+}
+
+resource "aws_lb_listener" "entradaLoadBalancer" {
+  load_balancer_arn = aws_lb.loadBalancer.arn
+  port = "8000"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alvoLoadBalancer.arn
   }
 }
