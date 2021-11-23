3e0ed33ef415486641eea7169a3b1fc90ab140d6
Leonardo de Macedo Sartorello <leo.sartorello98@gmail.com>
2021-11-23 17:38:32 +0000

    Aula_5

--- infra/main.tf
+++ infra/main.tf
@@ -22,8 +22,13 @@ resource "aws_launch_template" "maquina" {
     Name = "Terraform Ansible Python"
   }
   security_group_names = [ var.grupoDeSeguranca ]
-  user_data = filebase64("ansible.sh")
+  user_data = var.producao ? filebase64("ansible.sh") : ""
 }
+# if(var.producao){
+#   filebase64("ansible.sh")
+# } else{
+#   ""
+# }
 
 resource "aws_key_pair" "chaveSSH" {
   key_name = var.chave
@@ -36,7 +41,7 @@ resource "aws_autoscaling_group" "grupo" {
   name = var.nomeGrupo
   max_size = var.maximo
   min_size = var.minimo
-  target_group_arns = [ aws_lb_target_group.alvoLoadBalancer.arn ]
+  target_group_arns = var.producao ? [ aws_lb_target_group.alvoLoadBalancer[0].arn ] : []
   launch_template {
     id = aws_launch_template.maquina.id
     version = "$Latest"
@@ -54,6 +59,7 @@ resource "aws_default_subnet" "subnet_2" {
 resource "aws_lb" "loadBalancer" {
   internal = false
   subnets = [ aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id ]
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_default_vpc" "vpc" {
@@ -64,16 +70,18 @@ resource "aws_lb_target_group" "alvoLoadBalancer" {
   port = "8000"
   protocol = "HTTP"
   vpc_id = aws_default_vpc.vpc.id
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_lb_listener" "entradaLoadBalancer" {
-  load_balancer_arn = aws_lb.loadBalancer.arn
+  load_balancer_arn = aws_lb.loadBalancer[0].arn
   port = "8000"
   protocol = "HTTP"
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.alvoLoadBalancer.arn
+    target_group_arn = aws_lb_target_group.alvoLoadBalancer[0].arn
   }
+  count = var.producao ? 1 : 0
 }
 
 resource "aws_autoscaling_policy" "escala-Producao" {
@@ -86,4 +94,5 @@ resource "aws_autoscaling_policy" "escala-Producao" {
     }
     target_value = 50.0
   }
+  count = var.producao ? 1 : 0
 }
